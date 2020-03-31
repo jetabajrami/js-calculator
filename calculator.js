71cee772a91b625b7eda6b5cc6ce351d512bc59a
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-03-31 16:27:42 -0700

    Calcululator in JS

--- calculator.js
+++ calculator.js
@@ -1,5 +1,41 @@
 const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
+  num1: 4,
+  num2: 4,
+  operation: 'Add',
 }
+
+
+const calculator = function(input){
+
+  if(input.operation === "Add" || input.operation === "+"){
+    return input.num1 + input.num2;
+  }else if (input.operation === "Subtract" || input.operation === "-"){
+    return input.num1 - input.num2;
+  }else if (input.operation === "Multiply" || input.operation === "*"){
+    return input.num1 * input.num2;
+  }else if (input.operation === "Divide" || input.operation === "/"){
+    let result = input.num1 / input.num2;
+  }
+
+}
+
+
+const inputValidation = function(input){
+
+  const allOperator = ["Add","+", "Subtract","-", "Multiply", "*", "Divide", "/"]
+
+  if (typeof input.num1 != 'number'){
+    console.log(`${input.num1} is not a number, give me a number please!`)
+  } else if (typeof input.num2  != 'number'){
+    console.log(`${input.num2} is not a number give me a number please!`)
+  } else if (allOperator.includes(input.operation) === false){
+    console.log('This is not a valid operator');
+  } else if (allOperator.includes(input.operation)){
+    console.log(calculator(input));
+  }
+
+}
+
+
+inputValidation(exampleAdditionInput)
+ 
