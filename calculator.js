87d6090b8df6c07852ca4616eebce296cf809438
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-03-31 16:36:42 -0700

    fix a bug in inputValidation function and add some comments.

--- calculator.js
+++ calculator.js
@@ -1,10 +1,11 @@
+//Object
 const exampleAdditionInput = {
   num1: 4,
   num2: 4,
   operation: 'Add',
 }
 
-
+// Calculator.
 const calculator = function(input){
 
   if(input.operation === "Add" || input.operation === "+"){
@@ -16,26 +17,24 @@ const calculator = function(input){
   }else if (input.operation === "Divide" || input.operation === "/"){
     let result = input.num1 / input.num2;
   }
-
 }
 
-
+// If the input is valid or not.
 const inputValidation = function(input){
 
-  const allOperator = ["Add","+", "Subtract","-", "Multiply", "*", "Divide", "/"]
+  const allOperator = ["Add","+", "Subtract","-", "Multiply", "*", "Divide", "/"];
 
   if (typeof input.num1 != 'number'){
-    console.log(`${input.num1} is not a number, give me a number please!`)
+    console.log(`${input.num1} is not a number, give me a number please!`);
   } else if (typeof input.num2  != 'number'){
-    console.log(`${input.num2} is not a number give me a number please!`)
+    console.log(`${input.num2} is not a number give me a number please!`);
   } else if (allOperator.includes(input.operation) === false){
     console.log('This is not a valid operator');
   } else if (allOperator.includes(input.operation)){
-    console.log(calculator(input));
+    return calculator(input);
   }
-
 }
 
-
-inputValidation(exampleAdditionInput)
+//Calling function.
+console.log(`Total is:  ${inputValidation(exampleAdditionInput)}`);
  
